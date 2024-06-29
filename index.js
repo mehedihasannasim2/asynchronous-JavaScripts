861b6cf80d55afada55cad86e285b0270baf5feb
nasim-etlimited <nasim@etlimited.net>
2024-06-29 15:47:04 +0600

    Patterns for Dealing with Asynchronous Code

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('reading a user from a database');
-}, 2000);
+const user = getUser(1);
+console.log(user);
 console.log('After');
 
+function getUser(id) {
+    setTimeout(() => {
+        console.log('reading a user from a database');
+        return { id: id, githubUsername: 'nasim'};
+    }, 2000);
+
+    return 2;
+}
