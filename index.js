8c2852798ed3352699bcd18b60c65d7805ad14e0
nasim-etlimited <nasim@etlimited.net>
2024-06-29 18:09:40 +0600

    Callback function

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+const user = getUser(1, (user) => {
+    console.log('User', user);
+
+    // Get the repositories
+    gerRepositories(user.githubUsername, (repos) => {
+        console.log('Repos', repos);
+    });
+
+});
 console.log('After');
 
-function getUser(id) {
+// Three patterns to deal with asynchonous code. 
+// Callbacks
+// Promises
+// Async/ await
+
+
+function getUser(id, Callbacks) {
     setTimeout(() => {
         console.log('reading a user from a database');
-        return { id: id, githubUsername: 'nasim'};
+        Callbacks({ id: id, githubUsername: 'nasim'});
     }, 2000);
+}
 
-    return 2;
+function gerRepositories(username, Callbacks) {
+    setTimeout(() => {
+        console.log('calling github api...');
+        Callbacks(['repo1', 'repo2', 'repo3']);
+    }, 2000);
 }
