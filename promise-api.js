9292914409501f28eb492a2a2021135b2ec58ee6
nasim-etlimited <nasim@etlimited.net>
2024-07-02 16:04:17 +0600

    running promises in parallel

--- promise-api.js
+++ promise-api.js
@@ -1,7 +1,20 @@
 
-const p = Promise.resolve({ id: 1, username: 'nasim'});
-p.then(result => console.log(result));
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        console.log('Async Operation 1....');
+        // resolve(1);
+        reject(new Error('Because something is failed'));
+    }, 2000);
+});
 
 
-const p1 = Promise.reject(new Error('reason for rejection'));
-p1.catch(error => console.log(error));
+const p2 = new Promise((resolve) => {
+    setTimeout(()=> {
+        console.log('Async Operation 2....');
+        resolve(2);
+    }, 2000);
+});
+
+Promise.all([p1, p2])
+    .then(result => console.log(result))
+    .catch(err => console.log('Error', err.message));
